cc3601c4d168df2d976d81172b04ddf25687eb1d
Abhiram Chandrasenan <81793953+ModkaVartini@users.noreply.github.com>
2021-09-15 19:49:54 +0530

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -1,19 +1,21 @@
 $githubName="ModkaVartini"
 $skinName="Amarena"
-$latestVer="0.6"
-$currentVer="0.5"
+$latestVerRaw=Invoke-WebRequest -Uri "https://api.github.com/repos/$githubName/$skinName/releases/latest" -UseBasicParsing
+$latestVer=($latestVerRaw | ConvertFrom-Json).tag_name -replace "v",""
 $url="https://github.com/$githubName/$skinName/releases/download/v$latestVer/$skinName"+"_$latestVer.rmskin"
 $outPath="C:/Windows/Temp/$skinName"+"_$latestVer.rmskin"
 
-If($latestVer -NotMatch $currentVer) {
+    Write-Host "Downloading $skinName..." -ForegroundColor "Yellow"
     $wc=New-Object System.Net.WebClient
     $wc.DownloadFile($url, $outPath)
     Start-Process -Filepath $outPath
 
     If($Null -NotMatch (get-process "SkinInstaller" -ea SilentlyContinue)) {
+        Write-Host "Installing v$latestVer..." -ForegroundColor "Yellow"
         $wshell=New-Object -ComObject wscript.shell
+        Start-Sleep -s .5
         $wshell.AppActivate('Rainmeter Skin Installer')
         Start-Sleep -s 1
         $wshell.SendKeys('~')
+        Write-Host "> $skinName was installed successfully." -ForegroundColor "Green"    
     }
-}
