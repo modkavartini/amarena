4ab894a971f488a864ef255d81dd7f58b9277482
Abhiram Chandrasenan <81793953+ModkaVartini@users.noreply.github.com>
2021-09-15 21:11:50 +0530

    Update Installer.ps1

--- Installer.ps1
+++ Installer.ps1
@@ -13,11 +13,9 @@ $outPath="C:/Windows/Temp/$skinName"+"_$latestVer.rmskin"
     If($Null -NotMatch (get-process "SkinInstaller" -ea SilentlyContinue)) {
         Write-Host "Installing v$latestVer..." -ForegroundColor "Yellow"
         $wshell=New-Object -ComObject wscript.shell
-        #Start-Sleep -s 1
-        #$wshell.AppActivate('Rainmeter Skin Installer');
         Start-Sleep -s 1
-        #$wshell.SendKeys('~')
-        Else {
+        $wshell.AppActivate('Rainmeter Skin Installer');
+        Start-Sleep -s 1
+        $wshell.SendKeys('~')
         Write-Host "> The $skinName suite was installed successfully." -ForegroundColor "Green"
-        }
     }
