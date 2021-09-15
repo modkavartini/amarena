ff9f256fcff19465d0436dde77e5d35e6ad3d25d
Abhiram Chandrasenan <81793953+ModkaVartini@users.noreply.github.com>
2021-09-15 19:58:13 +0530

    Update Installer.ps1

--- Installer.ps1
+++ Installer.ps1
@@ -5,17 +5,17 @@ $latestVer=($latestVerRaw | ConvertFrom-Json).tag_name -replace "v",""
 $url="https://github.com/$githubName/$skinName/releases/download/v$latestVer/$skinName"+"_$latestVer.rmskin"
 $outPath="C:/Windows/Temp/$skinName"+"_$latestVer.rmskin"
 
-    Write-Host "Downloading $skinName..." -ForegroundColor "DarkYellow"
+    Write-Host "Downloading $skinName.rmskin..." -ForegroundColor "Yellow"
     $wc=New-Object System.Net.WebClient
     $wc.DownloadFile($url, $outPath)
     Start-Process -Filepath $outPath
 
     If($Null -NotMatch (get-process "SkinInstaller" -ea SilentlyContinue)) {
-        Write-Host "Installing v$latestVer..." -ForegroundColor "DarkYellow"
+        Write-Host "Installing v$latestVer..." -ForegroundColor "Yellow"
         $wshell=New-Object -ComObject wscript.shell
         Start-Sleep -s .5
         $wshell.AppActivate('Rainmeter Skin Installer')
         Start-Sleep -s 1
         #$wshell.SendKeys('~')
-        Write-Host "> The $skinName suite was installed successfully." -ForegroundColor "DarkGreen"    
+        Write-Host "> The $skinName suite was installed successfully." -ForegroundColor "Green"
     }
